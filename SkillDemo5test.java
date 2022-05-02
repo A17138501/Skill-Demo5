48df9ba3cf5b047910b556b1ee3cd8dd29aa1f6a
A17138501 <97008935+A17138501@users.noreply.github.com>
2022-05-01 23:37:49 -0700

    add code and test

---
+++ SkillDemo5test.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo5test {
+    @Test
+    public void addition() {
+        assertEquals(3, SkillDemo5.add(2,5));
+    }
+    
+}
